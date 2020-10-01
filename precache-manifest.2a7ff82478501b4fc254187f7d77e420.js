002a890a1f652e6a01db35406f313c81656b3237
Huynh Tien Luc <luc@cookingthebooks.com.au>
2020-10-01 15:54:58 +0700

    Updates

---
+++ precache-manifest.2a7ff82478501b4fc254187f7d77e420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72524636db21da33345464942ea20408",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "3187ac74f867eb845053",
+    "url": "/monster-rolodex/static/css/main.b41ce7df.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3187ac74f867eb845053",
+    "url": "/monster-rolodex/static/js/main.d12c6212.chunk.js"
+  },
+  {
+    "revision": "d2a9c49a159018779ca4",
+    "url": "/monster-rolodex/static/js/runtime-main.131bb20d.js"
+  }
+]);
